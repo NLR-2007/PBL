4ab292262df4250ecb17bab134514dd3d89ce755
nlr-2007 <nimmalokeshreddy24@gmail.com>
2025-12-01 07:29:15 +0530

    add firebase config

---
+++ js/firebaseconfig.js
@@ -0,0 +1,13 @@
+// js/firebaseConfig.js
+// This file contains your secret Firebase keys.
+// IMPORTANT: This file is ignored by GitHub using .gitignore
+
+window.firebaseConfig = {
+  apiKey: "AIzaSyB5ODKZqumfpjVbPxyjWNzqAETxmexC16Y",
+  authDomain: "pbl-nlr.firebaseapp.com",
+  projectId: "pbl-nlr",
+  storageBucket: "pbl-nlr.firebasestorage.app",
+  messagingSenderId: "475620936852",
+  appId: "1:475620936852:web:19b995695b36d3db63b881",
+  measurementId: "G-034D5GEX59"
+};
